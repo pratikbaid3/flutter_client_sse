56d3417d022b5823bcd677eb1e1d423810d347d3
pratikbaid3 <pratikbaid3@gmail.com>
2021-04-05 22:23:38 +0530

    refactor: 🚀 Release refactoring

--- lib/client_sse.dart
+++ lib/client_sse.dart
@@ -1,45 +1,37 @@
 library client_sse;
 
 import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
-
-class SSEResponse {
-  String id = '';
-  String event = '';
-  String data = '';
-  SSEResponse({@required this.data, @required this.id, @required this.event});
-}
+part 'sse_event_model.dart';
 
 class SSEClient {
-  SSEResponse sseResponse = new SSEResponse(data: '', id: '', event: '');
   static http.Client _client;
-  static void subscribeToSSE(BuildContext context, String url, String token) {
+  static Stream<SSEModel> subscribeToSSE(String url, String token) async* {
     print("--SUBSCRIBING TO SSE---");
-    try {
-      _client = http.Client();
-      var request = new http.Request("GET", Uri.parse(url));
-      request.headers["Cache-Control"] = "no-cache";
-      request.headers["Accept"] = "text/event-stream";
-      request.headers["Cookie"] = token;
-      Future<http.StreamedResponse> response = _client.send(request);
-      response.asStream().listen((streamedResponse) {
-        streamedResponse.stream.listen((data) {
-          final rawData = utf8.decode(data);
-          final event = rawData.split("\n")[1];
-          if (event != null && event != '') {
-            print(rawData);
-            SSEResponse(
-                id: rawData.split("\n")[0],
-                event: rawData.split("\n")[1],
-                data: rawData.split("\n")[2]);
+    while (true) {
+      try {
+        _client = http.Client();
+        var request = new http.Request("GET", Uri.parse(url));
+        request.headers["Cache-Control"] = "no-cache";
+        request.headers["Accept"] = "text/event-stream";
+        request.headers["Cookie"] = token;
+        Future<http.StreamedResponse> response = _client.send(request);
+        await for (final data in response.asStream()) {
+          await for (final d in data.stream) {
+            final rawData = utf8.decode(d);
+            final event = rawData.split("\n")[1];
+            if (event != null && event != '') {
+              yield SSEModel.fromData(rawData);
+            }
           }
-        });
-      });
-    } catch (e) {
-      print('---ERROR---');
-      print(e);
+        }
+      } catch (e) {
+        print('---ERROR---');
+        print(e);
+        yield SSEModel(data: '', id: '', event: '');
+      }
+      await Future.delayed(Duration(seconds: 1), () {});
     }
   }
 
