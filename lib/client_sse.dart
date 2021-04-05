ffe766c0d1e9194ee9ee8233f28cfcd0c5d86ce5
pratikbaid3 <pratikbaid3@gmail.com>
2021-04-05 20:31:34 +0530

    feat: Created model and consumer for sse

--- lib/client_sse.dart
+++ lib/client_sse.dart
@@ -1,7 +1,45 @@
 library client_sse;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+class SSEResponse {
+  String id = '';
+  String event = '';
+  String data = '';
+  SSEResponse({@required this.data, @required this.id, @required this.event});
+}
+
+class SSEClient {
+  SSEResponse sseResponse = new SSEResponse(data: '', id: '', event: '');
+  static http.Client _client;
+  static void subscribeToSSE(BuildContext context, String url, String token) {
+    print("--SUBSCRIBING TO SSE---");
+    try {
+      _client = http.Client();
+      var request = new http.Request("GET", Uri.parse(url));
+      request.headers["Cache-Control"] = "no-cache";
+      request.headers["Accept"] = "text/event-stream";
+      request.headers["Cookie"] = token;
+      Future<http.StreamedResponse> response = _client.send(request);
+      response.asStream().listen((streamedResponse) {
+        streamedResponse.stream.listen((data) {
+          final rawData = utf8.decode(data);
+          final event = rawData.split("\n")[1];
+          if (event != null && event != '') {
+            print(rawData);
+          }
+        });
+      });
+    } catch (e) {
+      print('---ERROR---');
+      print(e);
+    }
+  }
+
+  static void unsubscribeFromSSE() {
+    _client.close();
+  }
 }
