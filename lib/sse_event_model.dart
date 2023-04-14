4ce1664ef4d264203a97e6869f26d5ced67af65e
1148648777@qq.com <1148648777@qq.com>
2023-04-14 11:51:09 +0800

    refactor(types): Change type of fields from  to  for non-nullable initialization.

--- lib/sse_event_model.dart
+++ lib/sse_event_model.dart
@@ -2,11 +2,11 @@ part of flutter_client_sse;
 
 class SSEModel {
   //Id of the event
-  String? id = '';
+  String id = '';
   //Event name
-  String? event = '';
+  String event = '';
   //Event data
-  String? data = '';
+  String data = '';
   SSEModel({required this.data, required this.id, required this.event});
   SSEModel.fromData(String data) {
     id = data.split("\n")[0].split('id:')[1];
